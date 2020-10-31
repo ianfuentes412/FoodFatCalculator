9000b041d37da46d8c5fbba0c818d67b1645a763
ianfuentes412 <ianfuentes412@users.noreply.github.com>
2020-10-31 14:56:24 -0700

    Added Files

---
+++ Fat Percentage Calculator.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace FatPercentCalc
+{
+    partial class FatPercentageCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FatGrams = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TotalGrams = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FatCheck = new System.Windows.Forms.CheckBox();
+            this.CheckLowFatL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FatGrams
+            // 
+            this.FatGrams.Location = new System.Drawing.Point(301, 144);
+            this.FatGrams.Name = "FatGrams";
+            this.FatGrams.Size = new System.Drawing.Size(100, 22);
+            this.FatGrams.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Fat (grams)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total Amount of Calories (grams)";
+            // 
+            // TotalGrams
+            // 
+            this.TotalGrams.Location = new System.Drawing.Point(301, 205);
+            this.TotalGrams.Name = "TotalGrams";
+            this.TotalGrams.Size = new System.Drawing.Size(100, 22);
+            this.TotalGrams.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(355, 273);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(60, 17);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "             ";
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Your Fat Percentage:";
+            // 
+            // FatCheck
+            // 
+            this.FatCheck.AutoSize = true;
+            this.FatCheck.Location = new System.Drawing.Point(279, 348);
+            this.FatCheck.Name = "FatCheck";
+            this.FatCheck.Size = new System.Drawing.Size(190, 21);
+            this.FatCheck.TabIndex = 7;
+            this.FatCheck.Text = "Check if product is low fat";
+            this.FatCheck.UseVisualStyleBackColor = true;
+            // 
+            // CheckLowFatL
+            // 
+            this.CheckLowFatL.AutoSize = true;
+            this.CheckLowFatL.Location = new System.Drawing.Point(309, 308);
+            this.CheckLowFatL.Name = "CheckLowFatL";
+            this.CheckLowFatL.Size = new System.Drawing.Size(68, 17);
+            this.CheckLowFatL.TabIndex = 8;
+            this.CheckLowFatL.Text = "               ";
+            this.CheckLowFatL.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CheckLowFatL.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // FatPercentageCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CheckLowFatL);
+            this.Controls.Add(this.FatCheck);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TotalGrams);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FatGrams);
+            this.Name = "FatPercentageCalculator";
+            this.Text = "FatPercentageCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FatGrams;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TotalGrams;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox FatCheck;
+        private System.Windows.Forms.Label CheckLowFatL;
+    }
+}
+
---
+++ Fat Percentage Calculator.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Program accepts 2 number inputs from the user then outputs the percentage between the 2 numbers. Use can tick a checkbox to output if the product is "low fat"
+namespace FatPercentCalc
+{
+    public partial class FatPercentageCalculator : Form
+    {
+        public FatPercentageCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                // Resets the Label that prints the results
+                CheckLowFatL.Text = " ";
+                // gramsFat stores the weight of fat before conversion, gramsTotal holds the food's total amount of calories and gramsCal holds the amount of fat converted into calories
+                double gramsCal = 0;
+                double gramsTotal = 0;
+                double gramsFat = 0;
+                // Boolean that turns true if the inputs fit within the parameters of the program
+                bool validInput = false;
+
+            // Checks if the input can be parsed into a double
+            if (double.TryParse(TotalGrams.Text, out gramsTotal) && double.TryParse(FatGrams.Text, out gramsFat))
+            {
+                //Converts the weight of fat into grams
+                gramsCal = gramsFat * 9;
+
+                //Checks the values if valid
+                validInput = CheckInputs(gramsCal, gramsTotal);
+
+                if (validInput == true)
+                {
+                    //Displays the fat percentage of the food
+                    CalcAndDisplayOutput(gramsCal, gramsTotal);
+                }
+            }
+            else
+                {
+                //Appears when user dosent enter a number in either of the textboxes
+                MessageBox.Show("Error: Only numbers are accepted");
+                }
+        }
+
+
+        private bool CheckInputs(double gramsCal, double gramsTotal)
+        {
+
+            //if statements display an error message if either value is less than or equal to zero
+            if (gramsTotal <= 0)
+            {
+                MessageBox.Show("Error: Total Calories cannot be equal or less than zero");
+                return false;
+            }
+            //It is specified which value is less than or equal to zero as per textbook instructions
+            else if (gramsCal <= 0)
+            {
+                MessageBox.Show("Error: Fat cannot be equal or less than zero");
+                return false;
+            }
+
+            //The Calories in fat cannot be more than the total calories of the product
+            else if (gramsCal > gramsTotal)
+            {
+                MessageBox.Show("Error: Fat cannot have more calories than the total");
+                return false;
+            }
+            else
+            {
+                //The rest of the program will only continue when input passes all if statements
+                return true;
+            }
+        }
+
+        private void CalcAndDisplayOutput(double gramsCal, double gramsTotal)
+        {
+            const int hundred = 100;
+            const int lowfat = 30;
+            
+            //Converts the ratio of fat and total calories into a percentage
+            double percentages = gramsCal / gramsTotal * hundred;
+            //Displays the percentage in a label
+            outputLabel.Text = Convert.ToString(percentages) + "%";
+
+            //Checks if the checkbox is checked
+            if (FatCheck.Checked == true)
+            {
+                //Checks if the calculated percent is lesser or equal to 30
+                if (percentages <= lowfat)
+                {
+                    //Displays message in a anonther label
+                    CheckLowFatL.Text = "The product is low fat.";
+                }
+
+                else
+                {
+                    
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
